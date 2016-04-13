d5e1d0dfd5da63e4adff900f60904c3addfb0c4c
shaleman <shaleman@cisco.com>
2016-04-13 09:27:33 -0700

    fix tests, godep import, vagrantfile

--- Godeps/_workspace/src/github.com/contiv/vagrantssh/sshnode.go
+++ Godeps/_workspace/src/github.com/contiv/vagrantssh/sshnode.go
@@ -37,7 +37,6 @@ type SSHNode struct {
 	sshAddr   string
 	sshPort   string
 	config    *ssh.ClientConfig
-	client    *ssh.Client
 }
 
 // NewSSHNode intializes a ssh-client based node in a testbed
@@ -129,17 +128,13 @@ func (n *SSHNode) RunCommand(cmd string) error {
 func (n *SSHNode) RunCommandWithOutput(cmd string) (string, error) {
 	client, s, err := n.getClientAndSession()
 	if err != nil {
-		fmt.Printf("\nSSH client error: %v\n", err)
-		return "\nSSH client error\n", err
+		return "", err
 	}
 
 	defer client.Close()
 	defer s.Close()
 
 	output, err := s.CombinedOutput(newCmdStrWithSource(cmd))
-	if err != nil {
-		fmt.Printf("\nSSH command execution error: %v\n", err)
-	}
 	return string(output), err
 }
 
