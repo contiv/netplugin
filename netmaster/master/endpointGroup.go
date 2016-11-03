445cfca8fe9e633323042c23a95b560853989422
Gaurav Dalvi <gaurav1424@gmail.com>
2016-11-03 10:49:40 -0700

    Resolving contiv/ccn/issue-3
    Signed-off-by: Gaurav Dalvi <gaurav1424@gmail.com>

--- netmaster/master/endpointGroup.go
+++ netmaster/master/endpointGroup.go
@@ -54,6 +54,12 @@ func CreateEndpointGroup(tenantName, networkName, groupName string) error {
 		return err
 	}
 
+	// if aci mode allocate per-epg vlan. otherwise, stick to per-network vlan
+	aciMode, err := IsAciConfigured()
+	if err != nil {
+		return err
+	}
+
 	// read the network config
 	networkID := networkName + "." + tenantName
 	nwCfg := &mastercfg.CfgNetworkState{}
@@ -67,10 +73,14 @@ func CreateEndpointGroup(tenantName, networkName, groupName string) error {
 	// params for docker network
 	if GetClusterMode() == "docker" {
 		// Create each EPG as a docker network
-		err = docknet.CreateDockNet(tenantName, networkName, groupName, nwCfg)
-		if err != nil {
-			log.Errorf("Error creating docker network for group %s.%s. Err: %v", networkName, groupName, err)
-			return err
+		if aciMode && nwCfg.PktTagType == "vxlan" {
+			log.Info("Skipping docker network creation because of ACI mode and nwtype = %v", nwCfg.PktTagType)
+		} else {
+			err = docknet.CreateDockNet(tenantName, networkName, groupName, nwCfg)
+			if err != nil {
+				log.Errorf("Error creating docker network for group %s.%s. Err: %v", networkName, groupName, err)
+				return err
+			}
 		}
 	}
 	// assign unique endpoint group ids
@@ -103,12 +113,6 @@ func CreateEndpointGroup(tenantName, networkName, groupName string) error {
 	epgCfg.StateDriver = stateDriver
 	epgCfg.ID = mastercfg.GetEndpointGroupKey(groupName, tenantName)
 	log.Debugf("##Create EpGroup %v network %v tagtype %v", groupName, networkName, nwCfg.PktTagType)
-
-	// if aci mode allocate per-epg vlan. otherwise, stick to per-network vlan
-	aciMode, rErr := IsAciConfigured()
-	if rErr != nil {
-		return rErr
-	}
 
 	// Special handling for ACI mode
 	if aciMode {
@@ -166,9 +170,9 @@ func DeleteEndpointGroup(tenantName, groupName string) error {
 	}
 
 	// if aci mode we allocate per-epg vlan. free it here.
-	aciMode, aErr := IsAciConfigured()
-	if aErr != nil {
-		return aErr
+	aciMode, err := IsAciConfigured()
+	if err != nil {
+		return err
 	}
 
 	if aciMode {
