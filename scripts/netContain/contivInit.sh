3a9c1037b202c04e8b336ee3971f9f12e351f5b5
unclejack <unclejack@users.noreply.github.com>
2017-03-16 01:04:20 +0200

    scripts/netContain: fix the formatting of scripts
    Signed-off-by: Cristian Staretu <cristian.staretu@gmail.com>

--- scripts/netContain/contivInit.sh
+++ scripts/netContain/contivInit.sh
@@ -6,19 +6,16 @@ set -x
 
 source /contiv/scripts/contivRc
 
-if [ $IS_NETMASTER == 1 ]
-then
-   /contiv/bin/netmaster 2> /var/log/contiv/netmaster.errlog 1> /var/log/contiv/netmaster.log &
+if [ $IS_NETMASTER == 1 ]; then
+	/contiv/bin/netmaster 2>/var/log/contiv/netmaster.errlog 1>/var/log/contiv/netmaster.log &
 fi
 
-echo "$NETMASTER_IP  netmaster" > /etc/hosts
-echo "0.0.0.0 localhost" >> /etc/hosts
-export no_proxy="0.0.0.0, $NETMASTER_IP" 
+echo "$NETMASTER_IP  netmaster" >/etc/hosts
+echo "0.0.0.0 localhost" >>/etc/hosts
+export no_proxy="0.0.0.0, $NETMASTER_IP"
 
-if [ not $CONTIV_FWD_MODE == "routing" ] 
-then
-   CONTIV_FWD_MODE="bridged"
+if [ not $CONTIV_FWD_MODE == "routing" ]; then
+	CONTIV_FWD_MODE="bridged"
 fi
 
-/contiv/bin/netplugin -vtep-ip $VTEP_IP -vlan-if $VLAN_IF -fwd-mode $CONTIV_FWD_MODE 2> /var/log/contiv/netplugin.errlog 1> /var/log/contiv/netplugin.log &
-
+/contiv/bin/netplugin -vtep-ip $VTEP_IP -vlan-if $VLAN_IF -fwd-mode $CONTIV_FWD_MODE 2>/var/log/contiv/netplugin.errlog 1>/var/log/contiv/netplugin.log &
