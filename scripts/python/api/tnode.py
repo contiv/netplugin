56c6ec65c6a6d620e6073bab1f161356ead63910
Ranjith <rchirakk@users.noreply.github.com>
2017-10-18 14:12:04 -0700

    added sudo to delete ovs bridge
    Signed-off-by: Ranjith <rchirakk@users.noreply.github.com>

--- scripts/python/api/tnode.py
+++ scripts/python/api/tnode.py
@@ -142,7 +142,7 @@ class Node:
     # Cleanup all state created by netplugin
     def cleanupSlave(self):
         self.runCmd("docker ps -a | grep alpine | awk '{print $1}' | xargs -r docker rm -fv ")
-        self.runCmd("sudo ovs-vsctl list-br | grep contiv | xargs -I % ovs-vsctl del-br % >/dev/null 2>&1")
+        self.runCmd("sudo ovs-vsctl list-br | grep contiv | xargs -I % sudo ovs-vsctl del-br % >/dev/null 2>&1")
         self.runCmd("/sbin/ifconfig | grep -e vport | awk '{print $1}' | xargs -r -n1 -I{} sudo ip link delete {} type veth")
         self.runCmd("sudo rm -f /var/run/docker/plugins/netplugin.sock")
         self.runCmd("sudo rm -f /tmp/net*")
