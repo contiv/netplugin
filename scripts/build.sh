b55226ea960106192084a1575a68917daa62decc
Chris Plock <chrisplo@cisco.com>
2017-11-02 09:47:45 -0700

    shfmt build.sh and getGitVersion.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,7 +5,7 @@ set -euxo pipefail
 GIT_COMMIT=$(./scripts/getGitCommit.sh)
 
 # TODO(chrisplo): remove when contiv/install no longer needs
-echo $BUILD_VERSION > netplugin-version
+echo $BUILD_VERSION >netplugin-version
 
 PKG_NAME=github.com/contiv/netplugin/version
 GOGC=1500 go install -v \
--- scripts/getGitVersion.sh
+++ scripts/getGitVersion.sh
@@ -7,14 +7,14 @@ NIGHTLY_RELEASE=${NIGHTLY_RELEASE:-}
 
 # calculate version
 if command -v git &>/dev/null && git rev-parse &>/dev/null; then
-    GIT_COMMIT=$(git describe --tags --always 2>/dev/null || echo unknown)
-    if [ -n "$(git status --porcelain --untracked-files=no)" ]; then
-        GIT_COMMIT="$GIT_COMMIT-unsupported"
-    fi
-    VERSION=$GIT_COMMIT
+	GIT_COMMIT=$(git describe --tags --always 2>/dev/null || echo unknown)
+	if [ -n "$(git status --porcelain --untracked-files=no)" ]; then
+		GIT_COMMIT="$GIT_COMMIT-unsupported"
+	fi
+	VERSION=$GIT_COMMIT
 else
-    echo >&2 'error: unable to determine the git revision'
-    exit 1
+	echo >&2 'error: unable to determine the git revision'
+	exit 1
 fi
 
 # BUILD_VERSION overrides the git calculated version
@@ -25,7 +25,7 @@ fi
 if [ -z "$NIGHTLY_RELEASE" ]; then
 	VERSION="$VERSION"
 else
-    BUILD_TIME=$(date -u +%m-%d-%Y.%H-%M-%S.UTC)
+	BUILD_TIME=$(date -u +%m-%d-%Y.%H-%M-%S.UTC)
 	VERSION="$VERSION-$BUILD_TIME"
 fi
 
