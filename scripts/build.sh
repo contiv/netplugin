76098d8f98d5d638212e0266c57ec964374f4b73
unclejack <unclejack@users.noreply.github.com>
2017-11-22 19:05:55 +0200

    script/build.sh: build static binaries
    Signed-off-by: Cristian Staretu <cristian.staretu@gmail.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,9 +5,10 @@ set -euxo pipefail
 GIT_COMMIT=$(./scripts/getGitCommit.sh)
 
 PKG_NAME=github.com/contiv/netplugin/version
-GOGC=1500 go install -v \
+GOGC=1500 CGO_ENABLED=0 go install -v \
+	-a -installsuffix cgo \
 	-ldflags "-X $PKG_NAME.version=$BUILD_VERSION \
 	-X $PKG_NAME.buildTime=$(date -u +%m-%d-%Y.%H-%M-%S.UTC) \
 	-X $PKG_NAME.gitCommit=$GIT_COMMIT \
-	-s -w" \
+	-s -w -d" -pkgdir /tmp/foo-cgo \
 	$TO_BUILD
